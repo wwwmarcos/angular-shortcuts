1b88db02b4c043408176a679215a11fc27a75b39
default <dstepbw@gmail.com>
2016-07-05 00:23:28 -0300

    starting the validations

--- angular-shortcuts.js
+++ angular-shortcuts.js
@@ -13,13 +13,60 @@
     , _shortCuts = [];
     return provider;
 
-    function $get(){
-      return _shortCuts;
+    function shortCut(keys, config) {
+      _shortCuts.push(new ShortCut(keys, config));
+      return this;
     };
 
-    function shortCut(shortCut) {
-      _shortCuts.push(shortCut);
-      return this;
+    function ShortCut(keys, config){
+      var keys = buildKeys(keys);
+      validateKeys(keys);
+
+      var shortCut = {
+          keyOne : keys[0]
+        , keyTwo : keys[1]
+      };
+
+      return shortCut;
+    };
+
+    function buildKeys(keys){
+      return keys.split('+');
+    };
+
+    function validateKeys(keys){
+      if (keys.length > 2)
+        throw new InvalidKeysException();
+    };
+
+    function InvalidKeysException() {
+      return Error('More than two keys defined.');
+    };
+
+    // the $get function retuns the service
+    $get.$inject = ['$log', '$rootScope', '$document'];
+    function $get($log, $rootScope, $document){
+      var service = {
+        log : log
+      };
+
+      $document[0].onkeydown = onKeyDown; 
+
+      function onKeyDown(event){
+        if (event.target.nodeName === 'BODY')
+          handle(event);
+      };
+
+      function handle(event){
+        console.log('event {}', event);
+      };
+
+      // for test
+      function log(){
+        $log.info('wow', _shortCuts);
+      };
+      
+      return service;
     };
   };
 })();
